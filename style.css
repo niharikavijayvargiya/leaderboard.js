718e833125aeba8ae55ade121da672f6f7e306c0
niharikavijayvargiya <154603361+niharikavijayvargiya@users.noreply.github.com>
2024-03-13 23:28:42 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,145 @@
+/* Resetting default margin and padding */
+* {
+    margin: 0;
+    padding: 0;
+}
+
+/* General styling for the entire page */
+body {
+    text-align: center;
+    font-family: sans-serif;
+    min-height: 100vh;
+    color: #252525;
+    background: #fcfcfc;
+    font-weight: 300;
+    display: grid;
+    justify-content: center;
+    padding: 3em 0;
+}
+
+/* Main content container styling */
+main {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 2em;
+}
+
+/* Title styling */
+.main_title {
+    font-size: 1.5em;
+    width: 80%;
+}
+
+/* Form styling */
+.main_form {
+    display: grid;
+    gap: .6em;
+    justify-items: center;
+}
+
+/* Input field styling */
+input {
+    all: unset;
+    padding: .5em .8em;
+    width: 80%;
+    border: 1px solid lightgreen;
+    text-align: start;
+    color: #131313;
+}
+
+input:focus {
+    border: 3px solid green;
+}
+
+/* Error prompter styling */
+.main_error-prompter {
+    color: #ff7864;
+    font-weight: 700;
+    font-size: 1em;
+    display: none;
+}
+
+/* Button styling */
+button:not(.main_form-submit-btn) {
+    all: unset;
+    border-radius: 50%;
+    background: lightgreen;
+    width: 60px;
+    height: 60px;
+    transition: all .5s ease;
+}
+
+.main_form-submit-btn {
+    all: unset;
+    width: 10em;
+    background: lightgreen;
+    padding: .7em 0;
+    transition: all .5s ease;
+}
+
+button:active {
+    transform: scale(.95) translateY(5px);
+    filter: brightness(.9);
+}
+
+/* Scoreboard container styling */
+.main_scoreboard-wrapper {
+    width: 100%;
+    display: grid;
+    justify-items: center;
+    gap: 1em;
+}
+
+/* Individual scoreboard item styling */
+.main_scoreboard {
+    display: grid;
+    justify-items: center;
+    gap: .3em;
+    width: 90%;
+    background: lightgreen;
+    text-transform: uppercase;
+    padding: 1em 0;
+}
+
+/* Player name styling */
+.main_player-name {
+    font-weight: 400;
+}
+
+/* Timestamp styling */
+.main_time-stamp {
+    color: #686868;
+    font-size: .8em;
+}
+
+/* Button container styling */
+.main_scoreboard-btn-container {
+    margin: 1em;
+    display: flex;
+    gap: .5em;
+}
+
+/* Media query for larger screens */
+@media (min-width: 35.25em) {
+    .main_title {
+        font-size: 2.5rem;
+        font-weight: 500;
+        font-variant: small-caps;
+    }
+
+    .main_form {
+        display: flex;
+    }
+
+    .main_scoreboard {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+
+    input {
+        width: 10em;
+    }
+}
